f9a6f1a2f1dd0d9439994720ff6afe9913952c47
WayneWei228 <waynewei0228@gmail.com>
2022-08-09 20:15:06 -0700

    Update

---
+++ codechef/COOK/08COOLING.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+using namespace std;
+
+struct Solution {
+
+    void Solve() {
+
+    }
+};
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++) Solution().Solve();
+    return 0;
+} 
