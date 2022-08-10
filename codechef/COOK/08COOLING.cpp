2d89ce3831223ab9470cdf9dca0ec7ddfd486e37
Wayne_tx <waynewei0228@gmail.com>
2022-08-10 12:21:00 -0700

    update

--- codechef/COOK/08COOLING.cpp
+++ codechef/COOK/08COOLING.cpp
@@ -1,10 +1,35 @@
 #include <cstdio>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 struct Solution {
+    int N;
+    int ans = 0;
+    vector<int> cookies, racks;
 
     void Solve() {
-
+        scanf("%d", &N);
+        for (int i = 0; i < N; i++) {
+            int x;
+            scanf("%d", &x);
+            cookies.emplace_back(x);
+        }
+        for (int i = 0; i < N; i++) {
+            int x;
+            scanf("%d", &x);
+            racks.emplace_back(x);
+        }
+        sort(cookies.begin(), cookies.end());
+        sort(racks.begin(), racks.end());
+        int i = 0;
+        for (int j = 0; j < N; j++) {
+            if (cookies[i] <= racks[j]) {
+                ans += cookies[i];
+                i++;
+            }
+        }
+        printf("%d\n", ans);
     }
 };
 
