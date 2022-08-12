522e9cb2c884022f79cd63eb9ca1aa34d571505b
WayneWei228 <waynewei0228@gmail.com>
2022-08-12 02:39:24 -0700

    Update

--- codechef/COOK/06MUFFINS.cpp
+++ codechef/COOK/06MUFFINS.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <vector>
 using namespace std;
 
 /*
@@ -11,11 +12,44 @@ N 个点中找大于等于 g 个点组成的独立集即为 possible
 
 */
 
+struct Edge {
+    int V[2];
+};
 
 struct Solution {
+    int N, M, G;
+    vector<Edge> E;
+    vector<bool> Covered;
+
+    bool Put(int no) {
+        if (no == M) return true;
+        if (Covered[E[no].V[0]] || Covered[E[no].V[1]]) {
+            return Put(no + 1);
+        }
+        if (G == 0) return false;
+        G--;
+        for (int i = 0; i < 2; i++) {
+            Covered[E[no].V[i]] = true;
+            if (Put(no + 1)) return true;
+            Covered[E[no].V[i]] = false;
+        }
+        G++;
+        return false;
+    }
 
     void Solve() {
-
+        scanf("%d%d%d", &N, &M, &G);
+        E.resize(M);
+        for (int i = 0; i < M; i++) {
+            scanf("%d%d", &E[i].V[0], &E[i].V[1]);
+        }
+        Covered.resize(N);
+        G = N - G;
+        if (Put(0)) {
+            printf("Possible\n");
+        } else {
+            printf("Impossible\n");
+        }
     }
 };
 
