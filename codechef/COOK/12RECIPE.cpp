9054fdf01da17ad9dd4dfa3f74d73b0e18e4eeb2
Wayne_tx <waynewei0228@gmail.com>
2022-08-13 21:15:37 -0700

    Update

--- codechef/COOK/12RECIPE.cpp
+++ codechef/COOK/12RECIPE.cpp
@@ -1,10 +1,33 @@
 #include <cstdio>
+#include <vector>
 using namespace std;
 
 struct Solution {
+    int N;
+    vector<int> A;
+
+    int GCD (int a, int b) {
+        if (b == 0) return a;
+        else return GCD(b, a % b);
+    }
 
     void Solve() {
-        
+        scanf("%d", &N);
+        A.resize(N);
+        for (int i = 0; i < N; i++) {
+            scanf("%d", &A[i]);
+        }
+        int divisor = 0;
+        for (int i = 0; i < N; i++) {
+            divisor = GCD(A[i], divisor);
+        }
+        bool head = true;
+        for (int i = 0; i < N; i++) {
+            if (!head) printf(" ");
+            head = false;
+            printf("%d", A[i] / divisor);
+        }
+        printf("\n");
     }
 };
 
