e52f4cc5fd8cea7dfa16b377304da155a16d5488
Wayne_tx <waynewei0228@gmail.com>
2022-08-14 16:57:49 -0700

    Update

---
+++ codechef/COOK/14KNIGHTS.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+using namespace std;
+
+
+struct  Solution {
+    
+    void Solve() {
+
+    }
+};
+
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++) Solution().Solve();
+    return 0;
+}
--- template.cpp
+++ template.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 struct  Solution {
-    
+
     void Solve() {
 
     }
