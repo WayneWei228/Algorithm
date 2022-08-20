2607656d784ace60f07d36edf1fc2178616af7b5
WayneWei228 <waynewei0228@gmail.com>
2022-08-19 22:14:18 -0700

    Update

--- KickStart/2022/RoundD/qs2.cpp
+++ KickStart/2022/RoundD/qs2.cpp
@@ -1,9 +1,33 @@
 #include <cstdio>
+#include <vector>
 using namespace std;
 
 struct Solution {
+    int N, M, K;
+    vector<int> A, B;
+    vector<vector<vector<vector<int>>>> dp;
 
-    void Solve() {
+    void Solve(int caseNo) {
+        scanf("%d", &N);
+        A.resize(N + 1);
+        for (int i = 1; i <= N; i++) {
+            scanf("%d", &A[i]);
+        }
+        scanf("%d", &M);
+        B.resize(M + 1);
+        for (int i = 1; i <= M; i++) {
+            scanf("%d", &B[i]);
+        }
+        scanf("%d", &K);
+    
+    }
+// 1 2 3 4
+// 0 1 2 3
+    int Compute(int Ai, int Aj, int Bi, int Bj, int k) {
+        if ((Ai - 1 + N - Aj) > N || (Bi - 1 + M - Bj) > M) {
+            return -999;
+        }
+        if (k == 0) return 0;
 
     }
 };
@@ -11,6 +35,6 @@ struct Solution {
 int main() {
     int t;
     scanf("%d", &t);
-    for (int i = 1; i <= t; i++) Solution().Solve();
+    for (int i = 1; i <= t; i++) Solution().Solve(i);
     return 0;
-} 
+}
