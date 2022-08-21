e09f1f95a70f3978f4414ca1200c41c430e298f0
WayneWei228 <waynewei0228@gmail.com>
2022-08-21 05:10:21 -0700

    Update

--- KickStart/2022/RoundE/qs2.cpp
+++ KickStart/2022/RoundE/qs2.cpp
@@ -1,5 +1,5 @@
-#include <cstdio>
 #include <algorithm>
+#include <cstdio>
 #include <vector>
 using namespace std;
 
@@ -18,20 +18,43 @@ struct Solution {
         for (int i = 0; i < N; i++) {
             scanf("%d", &A[i]);
         }
-        for (int i = 0; i < answer.size(); i++) {
+        for (int i = 0; i < A.size(); i++) {
             vector<int> S(A);
-            sort(S.begin(), S.end(), [&](int a, int b){
-                return a < b;
-            });
-            for (int i = 0; i < S.size(); i++) {
-                if (S[i] < A[i] * 2) {
-                    answer[i] = S[i];
-                    break;
+            sort(S.begin(), S.end());
+            int l = 0, r = S.size() - 1;
+            while (l < r) {  // <= 2 * A[i]
+                int mid = l + (r - l) / 2 + 1;
+                if (S[mid] > A[i] * 2) {
+                    r = mid - 1;
+                } else {
+                    l = mid;
                 }
             }
-            if (S[i] == A[i]) {
-                
+            // printf("S[l] %d A[i] %d\n", S[l], A[i]);
+            if (S[l] == A[i]) {
+                if (l + 1 < S.size() && S[l + 1] == A[i]) {
+                    l++;
+                }
+                l--;
+                if (l < 0) {
+                    answer[i] = -1;
+                } else {
+                    answer[i] = S[l];
+                }
+            } else {
+                answer[i] = S[l];
             }
+            // if (S[l] == A[i]) {
+            //     while (S[l++] == A[i]);
+            //     l--;  // rightmost
+            //     if (l == 0) {
+            //         answer[i] = -1;
+            //     } else {
+            //         answer[i] = S[l - 1];
+            //     }
+            // } else {
+            //     answer[i] = S[l];
+            // }
         }
         printf("Case #%d:", caseNo);
         for (int i = 0; i < answer.size(); i++) {
