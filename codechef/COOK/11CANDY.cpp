29dcfa9a4b74282446fdc48271f11ba4a2b1e358
Wayne_tx <waynewei0228@gmail.com>
2022-08-27 21:34:29 -0700

    Update

--- codechef/COOK/11CANDY.cpp
+++ codechef/COOK/11CANDY.cpp
@@ -1,10 +1,26 @@
 #include <cstdio>
+#include <vector>
 
 using namespace std;
 
 struct Solution {
+    int N;
 
     void Solve() {
+        scanf("%d", &N);
+        vector<int> a, b;
+        a.resize(N);
+        b.resize(N);
+        for (int i = 0; i < N; i++) {
+            scanf("%d%d", &a[i], &b[i]);
+        }    
+        int aNeed, bNeed;
+        scanf("%d%d", &aNeed, &bNeed);
+        vector<int> P;
+        P.resize(N);
+        for (int i = 0; i < N; i++) {
+            P[i] = a[i] / aNeed * b[i] - bNeed
+        }
 
     }
 };
