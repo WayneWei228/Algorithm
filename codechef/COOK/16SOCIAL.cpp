17fc3d38d49c3489e5e735ebb6f20c9016c3c4e1
WayneWei228 <waynewei0228@gmail.com>
2022-09-12 08:55:20 -0700

    Update

---
+++ codechef/COOK/16SOCIAL.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+struct  Solution {
+
+    void Solve() {
+
+
+    }
+};
+
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++) Solution().Solve();
+    return 0;
+}
