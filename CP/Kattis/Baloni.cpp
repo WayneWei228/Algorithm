b0803b6356d6a8840640a5c33959517290cb8522
WayneWei228 <waynewei0228@gmail.com>
2023-09-06 08:45:05 -0400

    Update

--- CP/Kattis/Baloni.cpp
+++ CP/Kattis/Baloni.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-#include <unordered_set>
 using namespace std;
 
-int arr[1000005];
-
 int main() {
+    int count[1000005] = {0};
     int N;
+    int ans = 0;
     cin >> N;
-
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        if (count[x] == 0) {
+            count[x - 1]++;
+        } else {
+            count[x]--;
+            count[x - 1]++;
+        }
+    }
+    for (int i = 0; i < N + 1; i++) {
+        ans += count[i];
+    }
+    cout << ans << endl;
+    return 0;
 }
