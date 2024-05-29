ae0f9a4e280c63fc08504e9dbd35b063056e1e8a
WayneWei228 <waynewei0228@gmail.com>
2024-05-28 21:29:59 -0400

    to English

--- test.cpp
+++ test.cpp
@@ -1,16 +1,10 @@
 // #include <bits/stdc++.h>
-#include <iostream>
-#include <string>
-
+// #include <iostream>
+// #include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-double f(double x) {
-    return sqrt(29 * x + 7);
-}
 
 int main() {
-    char a = '\'';
-    string s = "\'hi\"";
-    cout << s << endl;
-    cout << a << endl;
+    cout << "hello world" << endl;
 }
--- 算法训练营/进阶/RMQ/SConstruct
+++
@@ -1,6 +0,0 @@
-import glob
-
-env = Environment(CCFLAGS=["-O2", "--std=c++20"], CPPPATH='/opt/homebrew/Cellar/botan/2.19.2/include/botan-2')
-lib = ['botan-2']
-for i in glob.glob('*.cpp'):
-    env.Program(i[0:-4] + '.bin', i, LIBS=lib, LIBPATH = ['/opt/homebrew/Cellar/botan/2.19.2/lib'])
